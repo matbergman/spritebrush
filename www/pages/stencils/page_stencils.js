9d67cee09dd1702e789a7c1166d2e6bed5df1fe9
Mat Bergman <matbergman@users.noreply.github.com>
2015-02-16 22:55:52 -0800

    Stencils - work on stencil sizing. TODO: Convert to sag for better screen fit

--- www/pages/stencils/page_stencils.js
+++ www/pages/stencils/page_stencils.js
@@ -40,8 +40,11 @@ $("#stencilFrame").on("taphold",function(){
 /* set character height so graphic fills entire screen */
 
 
+/*
 document.getElementById("char").style.height = heightForPages+"px";
 document.getElementById("char").style.width = widthForPages+"px";
+*/
+
 
 });
                 
@@ -52,12 +55,33 @@ function showStencil() {
     var selectedStencil = $("#button_selectStencil").attr("class");
     var selectedStencilSpeed = $("#visualSelect_stencilSpeedValue").val();
     var theStencil = document.getElementById("char");
-    
+   
+
+
+
+    var wideStencilArray = new Array("stencil_0056","stencil_0058","stencil_0059","stencil_0060","stencil_0062","stencil_0065","stencil_0066","stencil_0067","stencil_0077","stencil_0083","stencil_0084","stencil_0091","stencil_0092","stencil_0094","stencil_0098","stencil_0100","stencil_0101","stencil_0104","stencil_0107","stencil_0110","stencil_0112");
+    for (i=0;i<wideStencilArray.length;i++) {
+        if (selectedStencil == wideStencilArray[i]) {
+
+            $("#char").css('fontSize','50vh');
+            }
+        //else {$("#char").css('fontSize','24px');}
+        }
+
+            console.log($("#char").css('fontSize'));
+
+
+/*
+    console.log("w: "+$("#char").width()+"  h: "+$("#char").height() );
+
+    if ($("#char").width() > widthForPages) {}
+*/
+
+
+
     document.getElementById("char").style['-webkit-animation-duration'] = selectedStencilSpeed;
     theStencil.className = selectedStencil;
 
-
-//console.log("width: "+$("#char").width());
 
 
 
@@ -65,6 +89,7 @@ function showStencil() {
     $("#page_stencils").hide();
     $("#stencilFrame").show();
     $("#char").show();
+
 
 }
 
